9020d1833b533727c5c411069f9374ff83baa302
Ilona-Poltavets <20ilona.poltavets02@gmail.com>
2021-05-24 21:47:22 +0300

    Test is OK

--- src/com/company/StationTest.java
+++ src/com/company/StationTest.java
@@ -10,20 +10,19 @@ public class StationTest {
     private Station station;
     private SpaceShip ss1;
     private SpaceShip ss2;
-    private SpaceShip ss3;
     ArrayList<SpaceShip>_ships1 = new ArrayList<SpaceShip>();
     @Before
     public void SetUp() throws Exception {
         station = new Station(10,50,"Saturn");
         ss1 = new SpaceShip("qwerty",Races.Earthlings,10);
-        ss2 = new SpaceShip("qwerty1",Races.Khodakeytsi,10);
-        ss3 = new SpaceShip("qwerty2", Races.Turiantsi,10);
+        ss2 = new SpaceShip("qwerty1",Races.Khodakeytsi,100);
     }
 
     @Test
     public void stationManager() {
-        ss1.SpaceShipImport(station);
-        boolean actual = station.StationManager(ss2);
+        boolean actual = station.StationManager(ss1);
         Assert.assertEquals(true, actual);
+        boolean actual2 = station.StationManager(ss2);
+        Assert.assertEquals(false, actual2);
     }
 }
